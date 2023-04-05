5349c950a9b7b0e2d72f159f3d8d3c217688b93f
miyoshi <miyoshimas@otsukaele.jp>
2023-04-05 17:05:49 +0900

    2023-4-5-4

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -4,6 +4,7 @@ const configuration = new Configuration({
    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 
 });
+
 const openai = new OpenAIApi(configuration);
 
 export const sendMessage = async (chat: string) => {
