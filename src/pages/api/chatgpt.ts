9a430398188877dc7110ba6351b78b7f6b903701
miyoshi <miyoshimas@otsukaele.jp>
2023-05-16 15:37:13 +0900

    2023-5-16-2

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -1,6 +1,10 @@
 const { Configuration, OpenAIApi } = require("openai");
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+export type GPTMessage = {
+  role: "user" | "assistant" | "system";
+  content: string;
+};
 
 const configuration = new Configuration({
    apiKey: process.env.OPENAI_API_KEY,
@@ -8,17 +12,17 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+/*
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const chat = req.body.chat;
     const response = await sendMessage(chat);
     res.status(200).json({ message: response });
   }
-  
-  
 
 export const sendMessage = async (chat: string) => {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
+    // model: "gpt-4",
     temperature:0.7, // Default:0.7 
     max_tokens:1000,
     messages: [{ role: "system", content: "あなたは優秀なアシスタントです。" },{ role: "user", content: chat }],
@@ -26,4 +30,26 @@ export const sendMessage = async (chat: string) => {
   const ret_str = completion.data.choices[0].message.content;
   return ret_str;
 };
+*/
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const chat = req.body.chat;
+  const response = await sendMessage(chat);
+  res.status(200).json({ message: response.choices[0].message.content, usage: response.usage});
+}
+
+
+export const sendMessage = async (chat: GPTMessage[]) => {
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  // model: "gpt-4",
+  temperature:0.7, // Default:0.7 
+  max_tokens:1000,
+  messages: chat,
+});
+// const ret_str = completion.data.choices[0].message.content;
+const ret_obj = completion.data;  //2023/5/8 data部をごっそり返す total_tokensを参照するため
+return ret_obj;
+};
+
 
